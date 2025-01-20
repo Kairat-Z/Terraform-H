2964a84cacc0252a050c786886b61ad0e0dca7e0
Kairat-Z <zh.kairat2@gmail.com>
2025-01-20 11:43:14 -0600

    Update main.tf
    added comments

--- Homework2/main.tf
+++ Homework2/main.tf
@@ -9,3 +9,6 @@ resource "aws_s3_bucket" "kaizen-kai2" {
 resource "aws_s3_bucket" "kaizen-kai3" {
     bucket = "kaizen-kai3"
 }
+
+# terraform import aws_s3_bucket.kaizen-kai2 kaizen-kai2
+# terraform import aws_s3_bucket.kaizen-kai3 kaizen-kai3
