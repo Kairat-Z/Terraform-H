142ac13b248556fb867d56d798acb3db8e84fdca
EC2 Default User <ec2-user@ip-172-31-11-39.us-east-2.compute.internal>
2025-01-26 22:27:14 +0000

    12222

--- Homework5/ec2.tf
+++ Homework5/ec2.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   #availability_zone = "us-west-2c"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   user_data = file("apache.sh")
